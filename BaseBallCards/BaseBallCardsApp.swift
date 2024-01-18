5568737ff0f80ba9fdb15173230a322fbc3b958c
Tristan Lehmann <tlehmann@lcs.on.ca>
2024-01-18 14:41:43 -0500

    Initial Commit

---
+++ BaseBallCards/BaseBallCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseBallCardsApp.swift
+//  BaseBallCards
+//
+//  Created by Tristan Lehmann on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct BaseBallCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
